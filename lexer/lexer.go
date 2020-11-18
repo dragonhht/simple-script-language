ef6a1050dcfd6a688232ffbe5d4c1ba72e50a8ea
dragonhht <dragon_hht@163.com>
2020-11-18 23:31:31 +0800

    解析源文件

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,7 +2,8 @@ package lexer
 
 import (
 	"bufio"
-	"container/list"
+	"errors"
+	"fmt"
 	"regexp"
 )
 
@@ -11,18 +12,94 @@ const regexPat = `\s*((//.*)|([0-9]+)|("(\\"|\\\\|\\n|[^"])*")|[A-Z_a-z][A-Z_a-z
 // Lexer 词法分析器
 type Lexer struct {
 	pattern *regexp.Regexp // 正则对象
-	queue *list.List // 单词暂存列表
+	queue []Token // 单词暂存列表
 	hasMore bool // 是否还有为解析单词
-	reader *bufio.Reader // 内容读取器
+	reader *bufio.Scanner // 内容读取器
 }
 
 // NewLexer 创建Lexer对象
-func NewLexer(reader *bufio.Reader) *Lexer {
+func NewLexer(reader *bufio.Scanner) *Lexer {
 	pattern, _ := regexp.Compile(regexPat)
     return &Lexer{
 		pattern: pattern,
-		queue:   list.New(),
+		queue:   make([]Token, 10),
 		hasMore: true,
 		reader:  reader,
 	}
+}
+
+// ParseError 解析异常
+func ParseError() error {
+    return errors.New("")
+}
+
+// Read 从源代码源头逐一获取单词
+func (l *Lexer) Read() (Token, error) {
+	fill, err := l.fillQueue(0)
+	if err != nil {
+		return nil, err
+	}
+	if fill {
+		token := l.queue[0]
+		l.queue = l.queue[1:]
+		return token, nil
+	} else {
+		return EOF, nil
+	}
+}
+
+// fillQueue 将单词加载到暂存列表
+func (l *Lexer) fillQueue(index int) (bool, error) {
+	// 当需要获取超过缓存长度的单词时，进行加载
+	for {
+		if index < len(l.queue) {
+			break
+		}
+		if l.hasMore {
+			err := l.readLine()
+			if err != nil {
+				return false, err
+			}
+		} else {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+// readLine 逐行读取单词
+func (l *Lexer) readLine() error {
+	var line string
+	if l.reader.Scan() {
+		line = l.reader.Text()
+	} else {
+		// 已到最后，没有更多
+		l.hasMore = false
+		return nil
+	}
+	// TODO 如何获取行号
+	var lineNo = 0
+	pos := 0
+	endPos := len(line)
+	for {
+		matcherLine := line[pos:]
+		if pos >= endPos {
+			break
+		}
+		loc := l.pattern.FindIndex([]byte(matcherLine))
+		// 起始匹配
+		if loc[0] == 0 {
+			l.addToken(lineNo, matcherLine)
+			pos = loc[1]
+		} else {
+			return errors.New(fmt.Sprintf("bad token at line %d", lineNo))
+		}
+	}
+	l.queue = append(l.queue, NewIdToken(lineNo, EOL))
+	return nil
+}
+
+// addToken 创建并保存Token对象
+func (l *Lexer) addToken(lineNo int, lineStr string) {
+
 }
--- lexer/token.go
+++ lexer/token.go
@@ -6,124 +6,134 @@ import (
 	"strconv"
 )
 
+// TokenInterface 单词接口
+type Token interface {
+	GetLineNumber() int // 获取行号
+	IsIdentifier() bool // 是否为标识符(变量名、函数名、类名)
+	IsNumber() bool // 是否为整型字面量
+	IsString() bool // 是否为字符串字面量
+	GetNumber() (int, error) // 获取整型字面量的值
+	GetText() string // 获取字符串字面量的值
+}
+
 var (
 	EOF = NewToken(1)
 	EOL = "\\n"
 )
 
 // Token 词法分析的结果(单词)
-type Token struct {
+type AbstractToken struct {
 	lineNumber int //行号
 }
 
 // NewToken 创建一个新的Token
-func NewToken(line int) *Token {
-	return &Token{lineNumber: line}
+func NewToken(line int) AbstractToken {
+	return AbstractToken{lineNumber: line}
 }
 
 // GetLineNumber 获取行号
-func (t *Token) GetLineNumber() int {
+func (t AbstractToken) GetLineNumber() int {
 	return t.lineNumber
 }
 
 // IsIdentifier 是否为标识符(变量名、函数名、类名)
-func (t *Token) IsIdentifier() bool {
+func (t AbstractToken) IsIdentifier() bool {
 	return false
 }
 
 // IsNumber 是否为整型字面量
-func (t *Token) IsNumber() bool {
+func (t AbstractToken) IsNumber() bool {
 	return false
 }
 
 // IsString 是否为字符串字面量
-func (t *Token) IsString() bool {
+func (t AbstractToken) IsString() bool {
 	return false
 }
 
 // GetNumber 获取整型字面量的值
-func (t *Token) GetNumber() (int, error) {
+func (t AbstractToken) GetNumber() (int, error) {
 	return -1, errors.New("not number token")
 }
 
 // GetText 获取字符串字面量的值
-func (t *Token) GetText() string {
+func (t AbstractToken) GetText() string {
 	return ""
 }
 
 // NumToken 整型字面量的Token
 type NumToken struct {
-	*Token
+	AbstractToken
 	value int // 整型字面量的值
 }
 
 // NewNumToken 创建NumToken对象
-func NewNumToken(line, value int) *NumToken {
-	return &NumToken{
-		Token: NewToken(line),
+func NewNumToken(line, value int) NumToken {
+	return NumToken{
+		AbstractToken: NewToken(line),
 		value: value,
 	}
 }
 
 // IsNumber 是否为整型字面量
-func (n *NumToken) IsNumber() bool {
+func (n NumToken) IsNumber() bool {
 	return true
 }
 
 // GetText 获取字符串字面量的值
-func (n *NumToken) GetText() string {
+func (n NumToken) GetText() string {
 	return strconv.Itoa(n.value)
 }
 
 // GetNumber 获取整型字面量的值
-func (n *NumToken) GetNumber() (int, error) {
+func (n NumToken) GetNumber() (int, error) {
 	return n.value, nil
 }
 
 // IdToken 标志符类型的Token
 type IdToken struct {
-	*Token
+	AbstractToken
 	text string // 标识
 }
 
 // NewIdToken 创建IdToken对象
-func NewIdToken(line int, id string) *IdToken {
-	return &IdToken{
-		Token: NewToken(line),
+func NewIdToken(line int, id string) IdToken {
+	return IdToken{
+		AbstractToken: NewToken(line),
 		text:  id,
 	}
 }
 
 // IsIdentifier 是否为标识符(变量名、函数名、类名)
-func (i *IdToken) IsIdentifier() bool {
+func (i IdToken) IsIdentifier() bool {
 	return true
 }
 
 // GetText 获取字符串字面量的值
-func (i *IdToken) GetText() string {
+func (i IdToken) GetText() string {
 	return i.text
 }
 
 // StrToken 字符串字面量的Token
 type StrToken struct {
-	*Token
+	AbstractToken
 	literal string // 字符串值
 }
 
 // NewStrToken 创建StrToken对象
-func NewStrToken(line int, literal string) *StrToken {
-	return &StrToken{
-		Token:   NewToken(line),
+func NewStrToken(line int, literal string) StrToken {
+	return StrToken{
+		AbstractToken:   NewToken(line),
 		literal: literal,
 	}
 }
 
 // IsString 是否为字符串字面量
-func (s *StrToken) IsString() bool {
+func (s StrToken) IsString() bool {
 	return true
 }
 
 // GetText 获取字符串字面量的值
-func (s *StrToken) GetText() string {
+func (s StrToken) GetText() string {
 	return s.literal
 }
